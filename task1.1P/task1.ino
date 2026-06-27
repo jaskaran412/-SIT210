28ada675c4ca8b35ff43b4d470caaa0cb4f0cbcf
jaskaran412 <jaskaransharma7979@gmail.com>
2026-06-27 23:26:31 +0530

    Update Morse code blinking function to Jaskaran

--- task1.1P/task1.ino
+++ task1.1P/task1.ino
@@ -1,41 +1,58 @@
 const int ledPin = LED_BUILTIN;
 
 void setup() {
-  pinMode(ledPin, OUTPUT);  
+  pinMode(ledPin, OUTPUT);
 }
+
 void dot() {
   digitalWrite(ledPin, HIGH);
   delay(200);
   digitalWrite(ledPin, LOW);
   delay(200);
 }
+
 void dash() {
   digitalWrite(ledPin, HIGH);
   delay(600);
   digitalWrite(ledPin, LOW);
   delay(200);
 }
+
 void letterSpace() {
   delay(600);
 }
+
 void wordSpace() {
   delay(1400);
 }
-void blinkMorse_Tanish() {
-  // T: -
-  dash(); letterSpace();
+
+void blinkMorse_Jaskaran() {
+  // J: .---
+  dot(); dash(); dash(); dash(); letterSpace();
+
   // A: .-
   dot(); dash(); letterSpace();
-  // N: -.
-  dash(); dot(); letterSpace();
-  // I: ..
-  dot(); dot(); letterSpace();
+
   // S: ...
   dot(); dot(); dot(); letterSpace();
-  // H: ....
-  dot(); dot(); dot(); dot(); letterSpace();
+
+  // K: -.-
+  dash(); dot(); dash(); letterSpace();
+
+  // A: .-
+  dot(); dash(); letterSpace();
+
+  // R: .-.
+  dot(); dash(); dot(); letterSpace();
+
+  // A: .-
+  dot(); dash(); letterSpace();
+
+  // N: -.
+  dash(); dot(); letterSpace();
 }
+
 void loop() {
-  blinkMorse_Tanish();   
-  wordSpace();         
+  blinkMorse_Jaskaran();
+  wordSpace();
 }
